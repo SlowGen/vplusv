f5c6f6f4d76c168021114a9329e4ba8301788050
slowgen <kalibobailey@gmail.com>
2021-05-30 11:18:11 -0400

    Feat: React initialized with Collection component

---
+++ src/assets/scripts/components/Collection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Collection = (props) => {
+    const {product} = props;
+    return(
+        <div>
+            <h1>Hello World!</h1>
+            {product}
+        </div>
+    )
+}
+
+export default Collection;
